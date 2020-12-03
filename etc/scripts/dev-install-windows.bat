037d18be2be192cbca4de2ba931b303929edce71
Gabor Vasarhelyi <vasarhelyi@collmot.com>
2020-12-04 00:45:55 +0100

    dev windows install is hacked to install all dependencies of skybrush...

--- etc/scripts/dev-install-windows.bat
+++ etc/scripts/dev-install-windows.bat
@@ -2,7 +2,6 @@ cd %~dp0
 cd ..
 cd ..
 
-
 :: This will be needed for all users on Windows to have all local python dependencies available
 mkdir "c:\Users\%USERNAME%\AppData\Roaming\Blender Foundation\Blender\2.83\scripts\vendor\skybrush\"
 copy "modules\blender_helpers.py" "c:\Users\%USERNAME%\AppData\Roaming\Blender Foundation\Blender\2.83\scripts\vendor\skybrush\"
@@ -18,6 +17,13 @@ mkdir "c:\Users\%USERNAME%\AppData\Roaming\Blender Foundation\Blender\2.83\scrip
 copy "addons\io_export_skybrush.py" "c:\Users\%USERNAME%\AppData\Roaming\Blender Foundation\Blender\2.83\scripts\addons\"
 copy "addons\io_import_skybrush.py" "c:\Users\%USERNAME%\AppData\Roaming\Blender Foundation\Blender\2.83\scripts\addons\"
 
-:: This is only needed for developers with local skybrush and pyledctrl
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: This partis only needed for developers with local skybrush, pyledctrl and flockwave-logger
+mkdir "c:\Users\%USERNAME%\AppData\Roaming\Blender Foundation\Blender\2.83\scripts\vendor\skybrush"
 mklink /D "c:\Users\%USERNAME%\AppData\Roaming\Blender Foundation\Blender\2.83\scripts\vendor\skybrush\skybrush" "d:\%USERNAME%\git\skybrush\skybrush"
 mklink /D "c:\Users\%USERNAME%\AppData\Roaming\Blender Foundation\Blender\2.83\scripts\vendor\skybrush\pyledctrl" "d:\%USERNAME%\git\pyledctrl\src\pyledctrl"
+:: ugly: add skybrush dependency flockwave-logger and its external dependencies
+mkdir "c:\Users\%USERNAME%\AppData\Roaming\Blender Foundation\Blender\2.83\scripts\vendor\skybrush\flockwave"
+mklink /D "c:\Users\%USERNAME%\AppData\Roaming\Blender Foundation\Blender\2.83\scripts\vendor\skybrush\flockwave\logger" "d:\%USERNAME%\git\flockwave-logger\src\flockwave\logger"
+call pip install colorlog -t "c:\\Users\\%USERNAME%\\AppData\\Roaming\\Blender Foundation\\Blender\\2.83\\scripts\\vendor\\skybrush\\"
+call pip install hexdump -t "c:\\Users\\%USERNAME%\\AppData\\Roaming\\Blender Foundation\\Blender\\2.83\\scripts\\vendor\\skybrush\\"
