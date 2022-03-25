bdabe64aa0f648877b1c1d0181743185872dc9ad
Tamas Nepusz <ntamas@gmail.com>
2022-03-25 22:18:44 +0100

    fix: less noisy builder script

--- etc/scripts/create_blender_dist.sh
+++ etc/scripts/create_blender_dist.sh
@@ -3,6 +3,13 @@
 # Bash script that creates a single ZIP file containing the full code of
 # Skybrush Studio for Blender so it can simply be extracted in the
 # Blender addons folder.
+#
+# It also creates standalone executables for Blender if the Skybrush Studio
+# bootloader is accessible on your system.
+#
+# You need to install Python 3 and poetry in order to use this script; both
+# of them must be accessible on the system path in order for the script to
+# succeed.
 
 OUTPUT_DIR="./dist"
 TMP_DIR="./tmp"
@@ -17,6 +24,12 @@ REPO_ROOT="${SCRIPT_ROOT}/../.."
 
 cd "${REPO_ROOT}"
 
+# Check whether the bootloader is present on the system
+BOOTLOADER_DIR=${BOOTLOADER_DIR:-"${REPO_ROOT}/../sbstudio-bootloader/dist"}
+if [ ! -d "${BOOTLOADER_DIR}" -o ! -f "${BOOTLOADER_DIR}/sbstudio-bootloader-linux" ]; then
+    BOOTLOADER_DIR=
+fi
+
 # Extract the name of the project and the version number from pyproject.toml
 PROJECT_NAME=`cat pyproject.toml|grep ^name|head -1|cut -d '"' -f 2`
 VERSION=`cat pyproject.toml|grep ^version|head -1|cut -d '"' -f 2`
@@ -30,7 +43,9 @@ trap "rm -f requirements.txt" EXIT
 
 # Create virtual environment if it doesn't exist yet
 if [ ! -d .venv ]; then
+  echo -n "--> Creating virtual environment... "
   python3 -m venv .venv
+  echo "done."
 fi
 
 # Create build folder
@@ -39,17 +54,22 @@ rm -rf "${BUILD_DIR}"
 mkdir -p "${BUILD_DIR}"
 mkdir -p "${BUILD_DIR}/vendor/skybrush"
 
-.venv/bin/pip install -U pip wheel pyclean pyminifier
-.venv/bin/pip install -r requirements.txt -t "${BUILD_DIR}/vendor/skybrush"
+echo -n "--> Installing dependencies... "
+.venv/bin/pip install -q -U pip wheel pyclean pyminifier
+.venv/bin/pip install -q -r requirements.txt -t "${BUILD_DIR}/vendor/skybrush"
 rm -rf "${BUILD_DIR}/vendor/skybrush/bin"
+echo "done."
 
 # Copy our code as well
+echo -n "--> Copying addon code... "
 cp -r src/modules/sbstudio ${BUILD_DIR}/vendor/skybrush
 cp src/addons/io_export_skybrush_csv.py ${BUILD_DIR}
 cp src/addons/ui_skybrush_studio.py ${BUILD_DIR}
+echo "done."
 
 # Clean any __pycache__ and *.dist-info files
-.venv/bin/pyclean ${BUILD_DIR}
+echo -n "--> Cleaning up and minifying code... "
+.venv/bin/pyclean -q ${BUILD_DIR}
 rm -rf ${BUILD_DIR}/vendor/skybrush/*.dist-info
 
 # Strip the comments from the source code
@@ -60,48 +80,61 @@ if [ "x${MINIFY}" = x1 ]; then
     fi
   done
 fi
+echo "done."
 
 # Create a single ZIP
+echo -n "--> Creating ZIP addon... "
 ZIP_STEM="${PROJECT_NAME}-${VERSION}"
 rm -rf "${TMP_DIR}/${ZIP_STEM}"
 mkdir -p "${TMP_DIR}/${ZIP_STEM}"
 cp -r "${BUILD_DIR}"/* "${TMP_DIR}/${ZIP_STEM}"
-( cd "${TMP_DIR}/${ZIP_STEM}"; zip -r "../${ZIP_STEM}.zip" * )
+( cd "${TMP_DIR}/${ZIP_STEM}"; zip -q -r "../${ZIP_STEM}.zip" * )
 mv "${TMP_DIR}/${ZIP_STEM}.zip" "${OUTPUT_DIR}"
 rm -rf "${TMP_DIR}/${ZIP_STEM}"
+echo "done."
 
-# Create a single-file Python entry point
-cat ${BUILD_DIR}/ui_skybrush_studio.py | sed -n '/BLENDER ADD-ON INFO ENDS HERE/,$p' >${BUILD_DIR}/entrypoint.py
-cat >>${BUILD_DIR}/entrypoint.py <<EOF
+if [ "x${BOOTLOADER_DIR}" != x ]; then
+	echo -n "--> Creating executables... "
+
+    # Create a single-file Python entry point
+    cat ${BUILD_DIR}/ui_skybrush_studio.py | sed -n '/BLENDER ADD-ON INFO ENDS HERE/,$p' >${BUILD_DIR}/entrypoint.py
+    cat >>${BUILD_DIR}/entrypoint.py <<EOF
 
 register()
 
 from sbstudio.plugin.api import set_fallback_api_key, get_api
 set_fallback_api_key("NNAs8w.hApopcx8s68YZAuRAGofbboqzFwx7KikdT0Q")
 EOF
-PYTHONPATH=vendor .venv/bin/python -m stickytape.main ${BUILD_DIR}/entrypoint.py \
-	--add-python-path ${BUILD_DIR}/vendor/skybrush \
-	--add-python-module sbstudio.plugin.utils.platform \
-	--add-python-module natsort \
-	>${OUTPUT_DIR}/${ZIP_STEM}.py.orig
-if [ "x${MINIFY}" = x1 ]; then
-  .venv/bin/pyminifier --gzip ${OUTPUT_DIR}/${ZIP_STEM}.py.orig >${OUTPUT_DIR}/${ZIP_STEM}.py
-  rm ${OUTPUT_DIR}/${ZIP_STEM}.py.orig
+    PYTHONPATH=vendor .venv/bin/python -m stickytape.main ${BUILD_DIR}/entrypoint.py \
+        --add-python-path ${BUILD_DIR}/vendor/skybrush \
+        --add-python-module sbstudio.plugin.utils.platform \
+        --add-python-module natsort \
+        >${OUTPUT_DIR}/${ZIP_STEM}.py.orig
+    if [ "x${MINIFY}" = x1 ]; then
+      .venv/bin/pyminifier --gzip ${OUTPUT_DIR}/${ZIP_STEM}.py.orig >${OUTPUT_DIR}/${ZIP_STEM}.py
+      rm ${OUTPUT_DIR}/${ZIP_STEM}.py.orig
+    else
+      mv ${OUTPUT_DIR}/${ZIP_STEM}.py.orig ${OUTPUT_DIR}/${ZIP_STEM}.py
+    fi
+
+    # Attach the single-file entry point to the bootloader(s)
+    .venv/bin/python etc/scripts/_append_to_bootloader.py \
+        --bootloader-dir "${BOOTLOADER_DIR}" \
+        --output-dir ${OUTPUT_DIR} \
+        ${OUTPUT_DIR}/${ZIP_STEM}.py
+
+    # Remove the single-file entry point, not needed any more
+    rm ${OUTPUT_DIR}/${ZIP_STEM}.py
+
+	echo "done."
+
+    # Create macOS launcher app in a disk image
+	echo -n "--> Creating macOS disk image... "
+    etc/scripts/_build_macos_dmg.sh "${BUILD_DIR}" "${OUTPUT_DIR}" >/dev/null 2>/dev/null
+	echo "done."
 else
-  mv ${OUTPUT_DIR}/${ZIP_STEM}.py.orig ${OUTPUT_DIR}/${ZIP_STEM}.py
+	echo "[-] Skipping executables - no bootloader code present."
 fi
-
-# Attach the single-file entry point to the bootloader(s)
-.venv/bin/python etc/scripts/_append_to_bootloader.py \
-	--bootloader-dir "${REPO_ROOT}"/../sbstudio-bootloader/dist \
-	--output-dir ${OUTPUT_DIR} \
-	${OUTPUT_DIR}/${ZIP_STEM}.py
-
-# Remove the single-file entry point, not needed any more
-rm ${OUTPUT_DIR}/${ZIP_STEM}.py
-
-# Create macOS launcher app in a disk image
-etc/scripts/_build_macos_dmg.sh "${BUILD_DIR}" "${OUTPUT_DIR}"
 
 # Clean up after ourselves
 rm -rf "${BUILD_DIR}"
@@ -110,6 +143,8 @@ echo ""
 echo "------------------------------------------------------------------------"
 echo ""
 echo "Bundle created successfully in ${OUTPUT_DIR}/${ZIP_STEM}.zip"
-echo "Single-file Windows executable created successfully in ${OUTPUT_DIR}/${ZIP_STEM}-win64.exe"
-echo "macOS launcher created successfully in ${OUTPUT_DIR}"
+if [ "x${BOOTLOADER_DIR}" != x ]; then
+    echo "Single-file Windows executable created successfully in ${OUTPUT_DIR}/${ZIP_STEM}-win64.exe"
+    echo "macOS launcher created successfully in ${OUTPUT_DIR}"
+fi
 
