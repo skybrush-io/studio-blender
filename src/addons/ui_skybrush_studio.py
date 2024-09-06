b5dd4b52d71008785725d8e144778e38e54353ed
Tamas Nepusz <ntamas@gmail.com>
2024-09-06 15:59:31 +0200

    chore: bumped version to 3.4.2

--- src/addons/ui_skybrush_studio.py
+++ src/addons/ui_skybrush_studio.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Skybrush Studio",
     "author": "CollMot Robotics Ltd.",
     "description": "Extends Blender with UI components for drone show design",
-    "version": (3, 4, 1),
+    "version": (3, 4, 2),
     "blender": (3, 3, 0),
     "category": "Interface",
     "doc_url": "https://doc.collmot.com/public/skybrush-studio-for-blender/latest/",
