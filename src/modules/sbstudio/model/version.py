d7778b66a5b7ff6f320f6c2dc53bfbb14ab00f8b
Gábor Vásárhelyi <vasarhelyi@collmot.com>
2025-08-22 12:00:25 +0200

    feat/check-backend-version (#42)
    Co-authored-by: Tamás Nepusz <ntamas@gmail.com>

---
+++ src/modules/sbstudio/model/version.py
@@ -0,0 +1,36 @@
+from sbstudio.api.types import Version
+from sbstudio.plugin.constants import MINIMUM_BACKEND_VERSION
+
+_version: Version
+
+
+__all__ = (
+    "get_backend_version",
+    "is_backend_version_ok",
+    "update_backend_version",
+)
+
+
+def is_backend_version_ok() -> bool:
+    """Returns whether the stored backend version is above the
+    current minimum requirement."""
+    global _version
+
+    return _version >= MINIMUM_BACKEND_VERSION
+
+
+def get_backend_version() -> Version:
+    """Returns the version of the server backend."""
+    global _version
+
+    return _version
+
+
+def update_backend_version(version: Version) -> None:
+    """Updates the backend version object returned from a server API call."""
+    global _version
+
+    _version = version
+
+
+update_backend_version(MINIMUM_BACKEND_VERSION)
--- src/modules/sbstudio/plugin/operators/refresh_file_formats.py
+++ src/modules/sbstudio/plugin/operators/refresh_file_formats.py
@@ -1,7 +1,13 @@
 from bpy.types import Operator
 
 from sbstudio.model.file_formats import update_supported_file_formats_from_limits
+from sbstudio.model.version import (
+    get_backend_version,
+    is_backend_version_ok,
+    update_backend_version,
+)
 from sbstudio.plugin.api import call_api_from_blender_operator
+from sbstudio.plugin.constants import MINIMUM_BACKEND_VERSION
 
 __all__ = ("RefreshFileFormatsOperator",)
 
@@ -9,6 +15,9 @@ __all__ = ("RefreshFileFormatsOperator",)
 class RefreshFileFormatsOperator(Operator):
     """Queries the server for the list of file formats that are supported by
     the server.
+
+    Note that operator also checks the backend version in the background and
+    informs the user if the backend is outdated.
     """
 
     bl_idname = "skybrush.refresh_file_formats"
@@ -22,6 +31,15 @@ class RefreshFileFormatsOperator(Operator):
                 self, "server capabilities query"
             ) as api:
                 update_supported_file_formats_from_limits(api.get_limits())
+                update_backend_version(api.get_version())
         except Exception:
             return {"CANCELLED"}
+
+        if not is_backend_version_ok():
+            self.report(
+                {"WARNING"},
+                f"Skybrush Studio Server is outdated ({get_backend_version()}), "
+                f"update to {MINIMUM_BACKEND_VERSION} or above!",
+            )
+
         return {"FINISHED"}
