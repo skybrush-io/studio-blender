4a1644ab6fa62b3d53860133839b1051fcf4a53c
Gabor Vasarhelyi <vasarhelyi@collmot.com>
2025-10-26 09:16:56 +0100

    fix: make sure we use drone template only if it exists

--- src/modules/sbstudio/plugin/operators/migrations/use_common_material_for_all_drones.py
+++ src/modules/sbstudio/plugin/operators/migrations/use_common_material_for_all_drones.py
@@ -23,18 +23,21 @@ def add_object_info_to_shader_node_tree_of_drone_template() -> None:
     """Checks drone template and if it seems to be old, upgrade
     it to contain object info shader node as drone color source."""
     template = Templates.find_drone(create=False)
-    template_material = get_material_for_led_light_color(template)
-    if template_material is not None:
-        _, input = _get_shader_node_and_input_for_diffuse_color_of_material(
-            template_material
-        )
-        if not input.is_linked:
-            nodes = template_material.node_tree.nodes
-            links = template_material.node_tree.links
-            object_info_node = nodes.new("ShaderNodeObjectInfo")
-            links.new(object_info_node.outputs["Color"], input)
-        elif input.links[0].from_node.name != "Object Info":
-            raise SkybrushStudioAddonError("Template drone shader node tree mismatch")
+    if template:
+        template_material = get_material_for_led_light_color(template)
+        if template_material is not None:
+            _, input = _get_shader_node_and_input_for_diffuse_color_of_material(
+                template_material
+            )
+            if not input.is_linked:
+                nodes = template_material.node_tree.nodes
+                links = template_material.node_tree.links
+                object_info_node = nodes.new("ShaderNodeObjectInfo")
+                links.new(object_info_node.outputs["Color"], input)
+            elif input.links[0].from_node.name != "Object Info":
+                raise SkybrushStudioAddonError(
+                    "Template drone shader node tree mismatch"
+                )
 
 
 def set_all_shading_color_types_to_object() -> None:
@@ -49,32 +52,35 @@ def upgrade_drone_color_animations_and_drone_materials() -> None:
     """Moves drone color animation from shader nodes to object colors
     and replaces drone material with template material."""
     template = Templates.find_drone(create=False)
-    template_material = get_material_for_led_light_color(template)
-    drones = Collections.find_drones()
-    num_drones = len(drones.objects)
-    last_log = time()
-    for i, drone in enumerate(drones.objects):
-        material = get_material_for_led_light_color(drone)
-        if material:
-            # copy color animation from shader node to drone
-            node_tree = material.node_tree
-            if node_tree.animation_data:
-                node, input = _get_shader_node_and_input_for_diffuse_color_of_material(
-                    material
+    if template:
+        template_material = get_material_for_led_light_color(template)
+        drones = Collections.find_drones()
+        num_drones = len(drones.objects)
+        last_log = time()
+        for i, drone in enumerate(drones.objects):
+            material = get_material_for_led_light_color(drone)
+            if material:
+                # copy color animation from shader node to drone
+                node_tree = material.node_tree
+                if node_tree.animation_data:
+                    node, input = (
+                        _get_shader_node_and_input_for_diffuse_color_of_material(
+                            material
+                        )
+                    )
+                    index = node.inputs.find(input.name)
+                    data_path = f'nodes["{node.name}"].inputs[{index}].default_value'
+                    keyframes = get_keyframes(node_tree, data_path)
+                    ensure_action_exists_for_object(drone)
+                    set_keyframes(drone, "color", keyframes, interpolation="LINEAR")
+                # reset drone's material to the template material
+                drone.material_slots[0].material = template_material
+            if time() - last_log > 10 or i == num_drones - 1:
+                log.info(
+                    f"{(i + 1) / num_drones * 100:.1f}% ready, "
+                    f"{i + 1}/{num_drones} drones converted"
                 )
-                index = node.inputs.find(input.name)
-                data_path = f'nodes["{node.name}"].inputs[{index}].default_value'
-                keyframes = get_keyframes(node_tree, data_path)
-                ensure_action_exists_for_object(drone)
-                set_keyframes(drone, "color", keyframes, interpolation="LINEAR")
-            # reset drone's material to the template material
-            drone.material_slots[0].material = template_material
-        if time() - last_log > 10 or i == num_drones - 1:
-            log.info(
-                f"{(i + 1) / num_drones * 100:.1f}% ready, "
-                f"{i + 1}/{num_drones} drones converted"
-            )
-            last_log = time()
+                last_log = time()
 
 
 def needs_migration():
