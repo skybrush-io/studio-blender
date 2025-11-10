d17beab84ef62bdbb752b698b8ede8a2775a3529
Gabor Vasarhelyi <vasarhelyi@collmot.com>
2025-11-10 23:00:40 +0100

    fix: update minimum required backend version

--- src/modules/sbstudio/api/constants.py
+++ src/modules/sbstudio/api/constants.py
@@ -3,5 +3,5 @@ from .types import Version
 SKYBRUSH_STUDIO_SERVER_URL = "https://studio.skybrush.io/api/v1"
 """Root URL of the Skybrush Studio online server."""
 
-MINIMUM_BACKEND_VERSION = Version(2, 29, 0)
+MINIMUM_BACKEND_VERSION = Version(2, 30, 0)
 """The minimum version of the backend that is compatible with this version of the plugin."""
