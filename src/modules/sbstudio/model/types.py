ddfcfcb8481cb5d175552f809b59597c59b8256e
Tamas Nepusz <ntamas@gmail.com>
2026-01-17 14:50:19 +0100

    chore: migrate away from #: docstrig style

--- src/modules/sbstudio/model/types.py
+++ src/modules/sbstudio/model/types.py
@@ -4,17 +4,17 @@ from typing import TypeAlias
 __all__ = ("Coordinate3D", "RGBAColor", "Rotation3D")
 
 
-#: Type alias for simple 3D coordinates
 Coordinate3D: TypeAlias = tuple[float, float, float]
+"""Type alias for simple 3D coordinates."""
 
-#: Type alias for RGBA color tuples used by Blender
 RGBAColor: TypeAlias = tuple[float, float, float, float]
+"""Type alias for RGBA color tuples used by Blender."""
 
-#: Type alias for RGBA color components that can be mutated in-place
 MutableRGBAColor: TypeAlias = MutableSequence[float]
+"""Type alias for RGBA color components that can be mutated in-place."""
 
-#: Type alias for mutable or immutable RGBA colors
 RGBAColorLike: TypeAlias = RGBAColor | MutableRGBAColor
+"""Type alias for mutable or immutable RGBA colors."""
 
-#: Type alias for simple 3D rotations
 Rotation3D: TypeAlias = tuple[float, float, float]
+"""Type alias for simple 3D rotations."""
