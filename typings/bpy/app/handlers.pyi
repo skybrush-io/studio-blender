a2ff99701bae799bdd136509fc5f54a2b5f8ea8c
Tamas Nepusz <ntamas@gmail.com>
2026-01-24 22:56:21 +0100

    chore: more typing improvements

--- typings/bpy/app/handlers.pyi
+++ typings/bpy/app/handlers.pyi
@@ -1,6 +1,19 @@
-from typing import Callable, ParamSpec, TypeVar
+from typing import Callable, Generic, Iterable, ParamSpec, TypeVar
+
+from bpy.types import Depsgraph, Scene
 
 P = ParamSpec("P")
 T = TypeVar("T")
+
+Func = TypeVar("Func", bound=Callable[..., None])
+
+class HandlerList(Generic[Func]):
+    def append(self, func: Func) -> None: ...
+    def remove(self, func: Func) -> None: ...
+    def __iter__(self) -> Iterable[Func]: ...
+
+depsgraph_update_post: HandlerList[Callable[[Scene, Depsgraph], None]]
+frame_change_post: HandlerList[Callable[[Scene, Depsgraph], None]]
+load_post: HandlerList[Callable[[str], None]]
 
 def persistent(func: Callable[P, T]) -> Callable[P, T]: ...
