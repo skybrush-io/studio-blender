62a63193fb5066fb3f78a66a63686b92b056541f
Tamas Nepusz <ntamas@gmail.com>
2026-02-13 23:03:35 +0100

    refactor: more precise typing for bmesh.ops

--- typings/bmesh/ops.pyi
+++ typings/bmesh/ops.pyi
@@ -1,3 +1,28 @@
-def create_cone(*args, **kwargs) -> None: ...
-def create_icosphere(*args, **kwargs) -> None: ...
-def subdivide_edges(*args, **kwargs) -> None: ...
+from bmesh.types import BMEdgeSeq, BMesh
+from mathutils import Matrix
+
+def create_cone(
+    bm: BMesh,
+    cap_ends: bool = False,
+    cap_tris: bool = False,
+    segments: int = 0,
+    radius1: float = 0,
+    radius2: float = 0,
+    depth: float = 0,
+    matrix: Matrix | None = None,
+    calc_uvs: bool = False,
+) -> None: ...
+def create_icosphere(
+    bm: BMesh,
+    subdivisions: int = 0,
+    radius: float = 0,
+    matrix: Matrix | None = None,
+    calc_uvs: bool = False,
+) -> None: ...
+def subdivide_edges(
+    bm: BMesh,
+    edges: BMEdgeSeq,
+    smooth: float = 0,
+    cuts: int = 0,
+    use_grid_fill: bool = False,
+) -> None: ...
