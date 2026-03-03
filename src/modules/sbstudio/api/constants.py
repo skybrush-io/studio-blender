f2f8dd8acd5580ef610ab0347fc01d889bf4d78d
Gabor Vasarhelyi <vasarhelyi@collmot.com>
2026-03-03 11:28:57 +0100

    fix: update minimum backend version required for kmz export

--- src/modules/sbstudio/api/constants.py
+++ src/modules/sbstudio/api/constants.py
@@ -3,5 +3,5 @@ from .types import Version
 COMMUNITY_SERVER_URL = "https://studio.skybrush.io/api/v1"
 """Root URL of the Skybrush Studio community server."""
 
-MINIMUM_BACKEND_VERSION = Version(2, 35, 0)
+MINIMUM_BACKEND_VERSION = Version(2, 38, 0)
 """The minimum version of the backend that is compatible with this version of the plugin."""
