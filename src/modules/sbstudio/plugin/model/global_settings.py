d3382d411880e17de7a05d4c4805e3afb789f46f
Tamas Nepusz <ntamas@gmail.com>
2026-06-16 13:04:03 +0200

    Revert "fix: put preferences related to mode of operation inside a box"
    This reverts commit b2c053ff424222427483b3c5044be088836a4947.

--- src/modules/sbstudio/plugin/model/global_settings.py
+++ src/modules/sbstudio/plugin/model/global_settings.py
@@ -169,41 +169,43 @@ class DroneShowAddonGlobalSettings(AddonPreferences):
     def draw(self, context: Context) -> None:
         layout = self.layout
 
-        box = layout.box()
-
         # Header: mode of operation. Most other widgets depend on this.
         mode = self.operation_mode
         if mode not in ("COMMUNITY", "LOCAL", "CLOUD", "ADVANCED"):
             # Failsafe in case the user somehow managed to screw up this setting
             mode = "ADVANCED"
-        box.prop(self, "operation_mode")
+        layout.prop(self, "operation_mode")
 
         # Top separator not needed for the simple cases
         if mode not in ("COMMUNITY", "LOCAL"):
-            box.separator()
+            layout.separator()
 
         # Hardware ID and register button. Needed for the cloud-based solution only.
         if mode in ("CLOUD", "ADVANCED"):
-            self._draw_hardware_id_widgets(box)
+            self._draw_hardware_id_widgets()
 
         # Gateway URL. Only for advanced use-cases. Gateway URL implied for "CLOUD"
         # and empty for all other configurations.
         if mode == "ADVANCED":
-            self._draw_gateway_widgets(box)
+            self._draw_gateway_widgets()
 
         # API key. Not needed for local servers.
         if mode != "LOCAL":
-            box.prop(self, "api_key")
+            layout.prop(self, "api_key")
 
         # Server URL and shortcuts to set to predefined values. Only for advanced use-cases.
         if mode == "ADVANCED":
-            self._draw_server_url_widgets(box)
+            self._draw_server_url_widgets()
 
-        layout.separator()
+        # Bottom separator not needed for the simple cases
+        if mode not in ("COMMUNITY", "LOCAL"):
+            layout.separator()
 
         layout.prop(self, "enable_experimental_features")
 
-    def _draw_hardware_id_widgets(self, layout) -> None:
+    def _draw_hardware_id_widgets(self) -> None:
+        layout = self.layout
+
         row = layout.row()
         col = row.column()
         col.enabled = False
@@ -214,7 +216,9 @@ class DroneShowAddonGlobalSettings(AddonPreferences):
         col.enabled = bool(self.hardware_id)
         col.operator(RegisterHardwareIDOperator.bl_idname, text="Register")
 
-    def _draw_gateway_widgets(self, layout) -> None:
+    def _draw_gateway_widgets(self) -> None:
+        layout = self.layout
+
         row = layout.row()
         col = row.column()
         col.prop(self, "gateway_url")
@@ -236,7 +240,9 @@ class DroneShowAddonGlobalSettings(AddonPreferences):
 
         layout.separator()
 
-    def _draw_server_url_widgets(self, layout) -> None:
+    def _draw_server_url_widgets(self) -> None:
+        layout = self.layout
+
         layout.prop(self, "server_url")
 
         row = layout.row()
