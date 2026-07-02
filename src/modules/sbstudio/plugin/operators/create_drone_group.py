83c4835ae54d189a3c28bc4f6c0eb3797cc60683
Tamas Nepusz <ntamas@gmail.com>
2026-07-02 23:19:33 +0200

    style: nitpicking

--- src/modules/sbstudio/plugin/operators/create_drone_group.py
+++ src/modules/sbstudio/plugin/operators/create_drone_group.py
@@ -1,24 +1,23 @@
 import bpy
+from bpy.types import Context, Operator
 
 from sbstudio.plugin.constants import Collections
 
 __all__ = ("CreateDroneGroupOperator",)
 
 
-class CreateDroneGroupOperator(bpy.types.Operator):
+class CreateDroneGroupOperator(Operator):
     bl_idname = "skybrush.create_drone_group"
     bl_label = "Create a Drone Group"
-    bl_description = "Create a new drone group in the Drone Groups collection"
+    bl_description = "Create a new drone group in the drone groups collection"
     bl_options = {"REGISTER", "UNDO"}
 
-    drone_group: bpy.props.StringProperty(default="")  # type: ignore
-
     @classmethod
-    def poll(cls, context):
+    def poll(cls, context: Context):
         return context.scene.skybrush
 
-    def execute(self, context):
-        dronegroup_coll = bpy.data.collections.new("Drone Group")
+    def execute(self, context: Context):
+        dronegroup_coll = bpy.data.collections.new("New Drone Group")
         main_coll = Collections.find_drone_groups(create=True)
         main_coll.children.link(dronegroup_coll)
         return {"FINISHED"}
